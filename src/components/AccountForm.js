7e84eb63cb2a480e7c2f1fc2f674d6a8f87961a8
scburkha005 <scburkha005@gmail.com>
2022-02-28 21:23:56 -0800

    useState for username, password properly connected as values onChange to input fields

--- src/components/AccountForm.js
+++ src/components/AccountForm.js
@@ -1,13 +1,29 @@
 import { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams, Link } from 'react-router-dom';
 
 const AccountForm = ({ setToken }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   const { method } = useParams();
   const navigate = useNavigate();
   const accountMethod = method === 'login' ? 'Log In' : 'Register';
 
+  console.log('username', username, 'password', password);
   return (
-    <h2>{accountMethod}</h2>
+    <form className='account-form'>
+      <h2>{accountMethod}</h2>
+      <input placeholder='username' value={username} onChange={e => setUsername(e.target.value)}/>
+      <input placeholder='password' type='password' value={password} onChange={e => setPassword(e.target.value)}/>
+      <button>{accountMethod}</button>
+      <div>
+        {
+          method === 'login'
+            ? <Link to={'/account/register'}>Don't have an account? Sign Up</Link>
+            : <Link to={'/account/login'}>Already have an account? Log In</Link>
+        }
+      </div>
+    </form>
   )
 }
 
