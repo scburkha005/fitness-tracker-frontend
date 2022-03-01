f14c3558503b8385b1768683165d1e3c9f8e5d10
scburkha005 <scburkha005@gmail.com>
2022-02-28 21:01:36 -0800

    token useState, pass as prop to AccountForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import { useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import { Home, AccountForm } from './';
 
 const App = () => {
+  const [token, setToken] = useState('');
+
   return (
     <div className="App">
       <nav className='navbar'>
@@ -10,7 +13,7 @@ const App = () => {
       </nav>
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route exact path='/account/:method' element={<AccountForm />} />
+        <Route exact path='/account/:method' element={<AccountForm setToken={setToken}/>} />
       </Routes>
     </div>
   );
