f8351cd7a5e8b758dff7328fa9a917a06358520c
Brian Hogue <bhogue32@gmail.com>
2022-03-02 14:57:37 -0800

    Created the files.

---
+++ src/components/routines.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from "react";
+const callApi = () => {
+const [] = useState();
+
+return (
+    <>
+    <h3>Routine:</h3>
+    <h3>Goal:</h3>
+    <h3>Creator:</h3>
+    </>
+
+
+)
+}
