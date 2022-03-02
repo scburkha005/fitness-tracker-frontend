85aa97372b5e28657e8810551895b2748c163e20
scburkha005 <scburkha005@gmail.com>
2022-03-01 22:06:32 -0800

    useMutate to handleSubmit for register/login api calls

--- src/components/AccountForm.js
+++ src/components/AccountForm.js
@@ -1,24 +1,31 @@
 import { useState } from 'react';
 import { useNavigate, useParams, Link } from 'react-router-dom';
+import { useMutation } from 'react-query';
+import * as api from '../api/usersApi'
 
 const AccountForm = ({ setToken }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
+  const [userFields, setUserFields] = useState({username: '', password: ''})
   const { method } = useParams();
   const navigate = useNavigate();
   const accountMethod = method === 'login' ? 'Log In' : 'Register';
 
+  const { mutate } = useMutation(api[method]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    //dynamically calls login or register based on :method
+    // const { message, token, user: userResObj } = await api[method](username, password);
+    mutate(userFields);
   }
 
+  //destructure our fields state
+  const { username, password } = userFields;
   console.log('username', username, 'password', password);
   return (
     <form className='account-form' onSubmit={handleSubmit}>
       <h2>{accountMethod}</h2>
-      <input placeholder='username' value={username} onChange={e => setUsername(e.target.value)}/>
-      <input placeholder='password' type='password' value={password} onChange={e => setPassword(e.target.value)}/>
+      <input placeholder='username' value={username} onChange={e => setUserFields({...userFields, username: e.target.value})}/>
+      <input placeholder='password' type='password' value={password} onChange={e => setUserFields({...userFields, password: e.target.value})}/>
       <button>{accountMethod}</button>
       <div>
         {
