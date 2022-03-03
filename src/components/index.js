d10f7fdf0ed0ede75ffb9b07dffd7f657a92f2cd
scburkha005 <scburkha005@gmail.com>
2022-03-02 19:44:51 -0800

    reexport Activities component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Home } from './Home.js';
 export { default as AccountForm } from './AccountForm.js';
 export { default as ButtonLogout } from './ButtonLogout.js';
-export { default as Routines } from './routines.js'
+export { default as Routines } from './routines.js';
+export { default as Activities } from './activities.js';
