264c4182817b2ea108ccdd87e64858a201b3a608
Brian Hogue <bhogue32@gmail.com>
2022-03-02 21:34:53 -0800

    Made progress on routines.

---
+++ src/components/routineSingle.js
@@ -0,0 +1,27 @@
+const RoutineSingle = () => {
+
+
+
+
+    return (
+        <>
+            <h3>Creator:</h3>
+            <div>
+                {routine.creatorName}
+            </div>
+            <h3>Name:</h3>
+            <div>
+                {routine.name}
+            </div>
+            <h3>Goal:</h3>
+            <div>
+                {routine.goal}
+            </div>
+        </>
+
+
+
+
+    )
+}
+export default RoutineSingle;
--- src/components/routines.js
+++ src/components/routines.js
@@ -1,12 +1,37 @@
 import React, {useState, useEffect} from "react";
+import {fetchRoutines} from '../api';
+import {RoutineSingle} from 'RoutineSingle';
 const Routines = () => {
-
-
+    const [routines, setRoutines] = useState([])
+    const getRoutines = async () => {
+        setRoutines(await fetchRoutines())  
+    }
+    useEffect(() => {        
+        getRoutines()
+    }, [])
+    console.log(routines)
 return (
     <>
-    <h3>Routine:</h3>
-    <h3>Goal:</h3>
-    <h3>Creator:</h3>
+    {routines.map(routine => {
+
+        console.log(routine)
+        return (
+            <>
+            <h3>Creator:</h3>
+            <div>
+                {routine.creatorName}
+            </div>
+            <h3>Name:</h3>
+            <div>
+                {routine.name}
+            </div>
+            <h3>Goal:</h3>
+            <div>
+                {routine.goal}
+            </div>
+            </>
+        )
+    })}
     </>
 
 
