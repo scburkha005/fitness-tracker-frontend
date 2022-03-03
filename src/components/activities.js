2d773364ff45da3e13d0d64feee7b8c716e2eb57
scburkha005 <scburkha005@gmail.com>
2022-03-02 20:52:01 -0800

    button to navigate to /activities/add which contains form to add and all activities

--- src/components/activities.js
+++ src/components/activities.js
@@ -1,8 +1,10 @@
 import { useQuery } from 'react-query';
+import { useNavigate } from 'react-router';
 import { fetchActivities } from '../api/activitiesApi';
 import ActivitiesSingle from './ActivitiesSingle';
 
-const Activities = () => {
+const Activities = ({ token }) => {
+  const navigate = useNavigate();
 
   const { data: activities, isLoading, error } = useQuery('activities', async () => {
     const data = await fetchActivities();
@@ -23,6 +25,7 @@ const Activities = () => {
   }
   return (
     <>
+      {token && <button onClick={() => navigate('/activities/add')}>Add Activity</button>}
       {activities.length > 0 && activities.map((activity) => {
         return (
           <ActivitiesSingle key={`${activity.id}-${activity.name}`}activity={activity} />
