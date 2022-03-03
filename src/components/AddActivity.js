5a7aa3d43f0b264cd91845e8f7398cea36a0ddf4
scburkha005 <scburkha005@gmail.com>
2022-03-03 15:44:27 -0800

    changed to query to refetch, line 20 added conditional to only check length if activities exists

--- src/components/AddActivity.js
+++ src/components/AddActivity.js
@@ -1,34 +1,23 @@
-import { useEffect } from "react";
 import { useNavigate } from "react-router";
-import { useQueryClient } from "react-query";
+import { useQuery } from "react-query";
 import ActivitiesSingle from "./ActivitiesSingle";
 import ActivitiesForm from "./ActivitiesForm";
 import { fetchActivities } from "../api/activitiesApi";
 
 const AddActivity = ({ token }) => {
   const navigate = useNavigate();
-  //queryClient allows us to access our "global" scope cache
-  const queryClient = useQueryClient();
 
-  const activities = queryClient.getQueryData('activities') || [];
+  const { data: activities, isLoading } = useQuery('activities', async () => await fetchActivities());
 
-  // How are we going to properly fetch the data from the queryClient when the page
-  // is manually refreshed during an add, do we need to just make a new query?
-  //
-  // const refetch = async () => {
-  //   const data = await queryClient.fetchQuery('activities', fetchActivities);
-  //   console.log(data)
-  // }
-
-  // useEffect(() => {
-  //   refetch();
-  // }, [])
+  if (isLoading) {
+    return 'Loading...'
+  }
 
   return (
     <>
       <button onClick={() => navigate('/activities')}>Cancel</button>
       <ActivitiesForm token={token} />
-      {activities.length > 0 && activities.map((activity) => {
+      {activities?.length > 0 && activities.map((activity) => {
         return (
           <ActivitiesSingle key={`${activity.id}-${activity.name}`}activity={activity} />
         )
