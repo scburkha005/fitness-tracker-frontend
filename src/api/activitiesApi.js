13e10595acaad9e862002385ad231676b5ca9f52
scburkha005 <scburkha005@gmail.com>
2022-03-02 19:45:10 -0800

    api call GET /activities

---
+++ src/api/activitiesApi.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://evening-wave-11430.herokuapp.com/api/activities'
+});
+
+export const fetchRoutines = async () => {
+  try {
+    const { data } = await api.get('/');
+
+    return data;
+  } catch (err) {
+    throw err;
+  }
+}
