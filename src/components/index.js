3916cc14b5db5bd2437ec9e2408adc1562da9043
scburkha005 <scburkha005@gmail.com>
2022-03-02 20:27:23 -0800

    reexport AddActivity

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as AccountForm } from './AccountForm.js';
 export { default as ButtonLogout } from './ButtonLogout.js';
 export { default as Routines } from './routines.js';
 export { default as Activities } from './activities.js';
+export { default as AddActivity } from './AddActivity.js';
