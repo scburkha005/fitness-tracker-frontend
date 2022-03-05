d81b08283f01238cfd8cb12b04a85afbcd35ac8b
scburkha005 <scburkha005@gmail.com>
2022-03-04 18:53:06 -0800

    routes/link to MyRoutines

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import { Home, AccountForm, ButtonLogout, Routines, Activities, AddActivity } from './';
-import RoutinesByActivity from './RoutinesByActivity';
+import { Home, AccountForm, ButtonLogout, Routines, Activities, AddActivity, MyRoutines, RoutinesByActivity } from './';
 
 const App = () => {
   const [token, setToken] = useState('');
@@ -24,6 +23,9 @@ const App = () => {
   return (
     <div className="App">
       <nav className='navbar'>
+        {
+          token && <Link to='/myroutines'>My Routines</Link>
+        }
         <Link to='/activities'>Activities</Link>
         {
           token
@@ -35,7 +37,8 @@ const App = () => {
         <Route path='/' element={<Home />}/>
         <Route exact path='/account/:method' element={<AccountForm setToken={setToken}/>} />
         <Route path='/routines' element={<Routines />}/>
-        <Route path='activities' element={<Activities token={token}/>} />
+        <Route path='/activities' element={<Activities token={token}/>} />
+        <Route path='/myroutines' element={<MyRoutines />} />
         <Route exact path='/activities/add' element={<AddActivity token={token}/>} />
         <Route exact path='/activities/:activityId/routines' element={<RoutinesByActivity />} />
       </Routes>
