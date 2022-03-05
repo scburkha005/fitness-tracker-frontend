e9b3574d61a6b33b4941790444c97468b65fd2a8
scburkha005 <scburkha005@gmail.com>
2022-03-05 13:40:58 -0800

    fetch user object w token if it exists and prop drill down

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import { Home, AccountForm, ButtonLogout, Routines, Activities, AddActivity, MyRoutines, RoutinesByActivity, AddRoutine } from './';
+import { getUser } from '../api/usersApi';
 
 const App = () => {
   const [token, setToken] = useState('');
+  const [user, setUser] = useState({});
 
   //grab token from localStorage if it exists
   useEffect(() => {
@@ -13,10 +15,15 @@ const App = () => {
     }
   }, [])
 
+  //set userObject when token exists
+  const fetchUser = async (token) => {
+    setUser(await getUser(token))
+  }
   //set token into localStorage if it exists
   useEffect(() => {
     if (token) {
       localStorage.setItem('token', token);
+      fetchUser(token);
     }
   }, [token])
 
@@ -37,7 +44,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route exact path='/account/:method' element={<AccountForm setToken={setToken}/>} />
-        <Route path='/routines' element={<Routines token={token}/>}/>
+        <Route path='/routines' element={<Routines token={token} user={user}/>}/>
         <Route exact path='/routines/add' element={<AddRoutine />} />
         <Route path='/activities' element={<Activities token={token}/>} />
         <Route path='/myroutines' element={<MyRoutines token={token}/>} />
