2b695279fd72bc980eef2a88f780551446654491
scburkha005 <scburkha005@gmail.com>
2022-03-05 15:34:03 -0800

    properly edit a routine and update its value in our routines useState

--- src/components/EditRoutine.js
+++ src/components/EditRoutine.js
@@ -1,7 +1,25 @@
-const EditRoutine = () => {
+import { useParams } from "react-router";
+import { updateRoutine } from "../api/routinesApi";
+import RoutinesForm from "./RoutinesForm";
 
+const EditRoutine = ({ routines, editRoutine, setEditRoutine, token }) => {
+  let { routineid: routineId } = useParams();
+  const { name, goal } = editRoutine;
+  routineId = routineId * 1;
+
+  const handleEdit = async (e) => {
+    e.preventDefault();
+    const fields = { name, goal };
+    const editedRoutine = await updateRoutine({ routineId, fields, token });
+    for (let i = 0; i < routines.length; i++) {
+      if (routines[i].id === routineId) {
+        routines[i] = editedRoutine;
+      }
+    }
+  }
   return (
     <>
+      <RoutinesForm handleSubmit={handleEdit} singleRoutine={editRoutine} setSingleRoutine={setEditRoutine}/>
     </>
   )
 }
