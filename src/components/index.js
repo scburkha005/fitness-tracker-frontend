b07a167f6a02c08b5fdad11c77370fcb4494001f
scburkha005 <scburkha005@gmail.com>
2022-03-04 18:52:09 -0800

    reexport

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,5 @@ export { default as ButtonLogout } from './ButtonLogout.js';
 export { default as Routines } from './routines.js';
 export { default as Activities } from './activities.js';
 export { default as AddActivity } from './AddActivity.js';
+export { default as MyRoutines } from './MyRoutines.js';
+export { default as RoutinesByActivity } from './RoutinesByActivity.js';
