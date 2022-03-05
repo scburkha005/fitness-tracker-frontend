a333b280cc95e73e068b4b281b1d54d10be74126
scburkha005 <scburkha005@gmail.com>
2022-03-05 14:25:21 -0800

    added form for edit and delete button

--- src/components/routineSingle.js
+++ src/components/routineSingle.js
@@ -1,24 +1,29 @@
+import { useNavigate } from "react-router";
 import ActivitiesSingle from "./ActivitiesSingle";
 import './routineSingle.css';
 
 const RoutineSingle = ({routine, user}) => {
+    const navigate = useNavigate();
 
-    console.log(routine, user)
     return (
-        <div className="routine-single">
-            <div>Name:</div>
-            <div>
-                {routine.name}
+        <>
+            <div className="routine-single">
+                <div>Name:</div>
+                <div>{routine.name}</div>
+                <div>Goal:</div>
+                <div>{routine.goal}</div>
+                {routine.activities.map(activity => {
+                return <ActivitiesSingle key={activity.id} activity={activity}/>
+            })}
+                <div>Created By: {routine.creatorName}</div>
             </div>
-            <div>Goal:</div>
-            <div>
-                {routine.goal}
-            </div>
-            {routine.activities.map(activity => {
-            return <ActivitiesSingle key={activity.id} activity={activity}/>
-           })}
-            <div>Created By: {routine.creatorName}</div>
-        </div>
+            { user &&
+            <form className="routine-single-button-form">
+                {user.id === routine.creatorId && <button onClick={() => navigate(`/routines/edit/${routine.id}`)} className='routine-single-edit-button'>Edit</button>}
+                {user.id === routine.creatorId && <button className='routine-single-delete-button'>Delete</button>}
+            </form>
+            }
+        </> 
     )
 }
 
