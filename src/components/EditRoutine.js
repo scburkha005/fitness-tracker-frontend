61fd3cb21ae3a6121c9199a06a3da7dc10ab7469
scburkha005 <scburkha005@gmail.com>
2022-03-06 13:29:53 -0800

    conditional render of edit form if user.id exists

--- src/components/EditRoutine.js
+++ src/components/EditRoutine.js
@@ -4,7 +4,7 @@ import AddActivityToRoutineForm from "./AddActivityToRoutineForm";
 import RoutinesForm from "./RoutinesForm";
 import RoutineSingle from "./routineSingle";
 
-const EditRoutine = ({ routines, editRoutine, setEditRoutine, token }) => {
+const EditRoutine = ({ routines, editRoutine, setEditRoutine, token, user }) => {
   const navigate = useNavigate();
   let { routineid: routineId } = useParams();
   const { name, goal } = editRoutine;
@@ -24,13 +24,17 @@ const EditRoutine = ({ routines, editRoutine, setEditRoutine, token }) => {
 
   return (
     <>
-      <h2>Editing Routine: </h2>
-      <button onClick={() => navigate(-1)}>Cancel</button>
-      <div className='routines'>
-        <RoutineSingle routine={editRoutine}/>
-      </div>
-      <RoutinesForm handleSubmit={handleEdit} singleRoutine={editRoutine} setSingleRoutine={setEditRoutine}/>
-      <AddActivityToRoutineForm routineId={routineId} token={token} editRoutine={editRoutine} setEditRoutine={setEditRoutine}/>
+    {user.id &&
+      <>
+        <h2>Editing Routine: </h2>
+        <button onClick={() => navigate(-1)}>Cancel</button>
+        <div className='routines'>
+          <RoutineSingle routine={editRoutine}/>
+        </div>
+        <RoutinesForm handleSubmit={handleEdit} singleRoutine={editRoutine} setSingleRoutine={setEditRoutine}/>
+        <AddActivityToRoutineForm routineId={routineId} token={token} editRoutine={editRoutine} setEditRoutine={setEditRoutine}/>
+      </>
+    }
     </>
   )
 }
