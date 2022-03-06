6e47e49d93848588eec32061bcce4248800628cf
scburkha005 <scburkha005@gmail.com>
2022-03-06 12:06:02 -0800

    select logic to dropdown through list of all activities

--- src/components/AddActivityToRoutineForm.js
+++ src/components/AddActivityToRoutineForm.js
@@ -1,11 +1,19 @@
 import { useState } from 'react';
+import { useQuery } from 'react-query';
 import { updateState } from './utils';
+import { fetchActivities } from '../api/activitiesApi';
 
-const AddActivityToRoutine = () => {
+const AddActivityToRoutineForm = ({ routineId, token}) => {
   const [routineFields, setRoutineFields] = useState({activityId: 0, count: 0, duration: 0});
+
+  const { data: activities } = useQuery('activities', async () => await fetchActivities());
 
   const handleChange = (e) => {
     updateState(e, routineFields, setRoutineFields)
+  }
+
+  const handleSubmit = async (e) => {
+
   }
 
   const { activityId, count, duration } = routineFields;
@@ -13,15 +21,18 @@ const AddActivityToRoutine = () => {
   return (
     <>
       <h2>Add Activities to Routine</h2>
-      <form>
-        <select>
-        
+      <form onSubmit={handleSubmit}>
+        <select name='activityId' value={activityId} onChange={handleChange}>
+            {activities?.map(activity => {
+              return <option value={activity.id} key={activity.id}>{activity.name}</option>
+            })}
         </select>
         <input value={count} placeholder='count' type='number' onChange={handleChange}/>
         <input value={duration} placeholder='duration' type='number' onChange={handleChange}/>
+        <button>Add Activity</button>
       </form>
     </>
   )
 }
 
-export default AddActivityToRoutine;
+export default AddActivityToRoutineForm;
