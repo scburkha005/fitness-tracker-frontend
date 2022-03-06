19afa6db4e84080a223dd39120b8e5ede2f81787
scburkha005 <scburkha005@gmail.com>
2022-03-06 12:05:28 -0800

    AddActivityToRoutineForm added

--- src/components/EditRoutine.js
+++ src/components/EditRoutine.js
@@ -1,6 +1,6 @@
 import { useParams, useNavigate } from "react-router";
 import { updateRoutine } from "../api/routinesApi";
-import AddActivityToRoutine from "./AddActivityToRoutineForm";
+import AddActivityToRoutineForm from "./AddActivityToRoutineForm";
 import RoutinesForm from "./RoutinesForm";
 import RoutineSingle from "./routineSingle";
 
@@ -30,7 +30,7 @@ const EditRoutine = ({ routines, editRoutine, setEditRoutine, token }) => {
         <RoutineSingle routine={editRoutine}/>
       </div>
       <RoutinesForm handleSubmit={handleEdit} singleRoutine={editRoutine} setSingleRoutine={setEditRoutine}/>
-      <AddActivityToRoutine />
+      <AddActivityToRoutineForm routineId={routineId} token={token} />
     </>
   )
 }
