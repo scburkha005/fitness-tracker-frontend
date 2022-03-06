6e7968030d51b37b85ecb412ddac6c1a8683a063
scburkha005 <scburkha005@gmail.com>
2022-03-05 16:53:50 -0800

    mutation to handle delete

--- src/components/MyRoutines.js
+++ src/components/MyRoutines.js
@@ -1,8 +1,11 @@
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
+import { deleteRoutine } from '../api/routinesApi';
 import * as api from '../api/usersApi';
 import RoutineSingle from './routineSingle';
+import { useQueryClient } from 'react-query';
 
-const MyRoutines = ({ token }) => {
+const MyRoutines = ({ token, setEditRoutine }) => {
+  const queryClient = useQueryClient();
 
   const { data: user , isLoading } = useQuery({
     enabled: Boolean(token),
@@ -17,22 +20,30 @@ const MyRoutines = ({ token }) => {
     queryFn: async () => await api.getRoutinesByUsername(user)
   });
 
+  //mutate to delete routine
+  const { mutate } = useMutation(deleteRoutine, {
+    onSuccess: (deletedRoutine) => {
+      const updatedRoutines = routines.filter(routine => routine.id !== deletedRoutine.id)
+      queryClient.setQueryData(['routinesByUsername', user], updatedRoutines);
+    }
+  })
+
   if (isLoading || loadingRoutines) {
     return "Loading..."
   }
 
   return (
-    <>
+    <div className='routines'>
       {
         routines?.length > 0 ?
         routines.map((routine) => {
           return (
-            <RoutineSingle routine={routine}/>
+            <RoutineSingle key={`${routine.id}-${routine.name}`} routine={routine} user={user} setEditRoutine={setEditRoutine} mutate={mutate} token={token}/>
           )
         })
         : <div>You haven't created any routines yet</div>
       }
-    </>
+    </div>
   )
 }
 
