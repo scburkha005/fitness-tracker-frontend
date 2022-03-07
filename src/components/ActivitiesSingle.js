f4c00caed4dd8b74e1644a5838adf5e868542be4
scburkha005 <scburkha005@gmail.com>
2022-03-06 17:42:00 -0800

    removed unneeded comment

--- src/components/ActivitiesSingle.js
+++ src/components/ActivitiesSingle.js
@@ -34,7 +34,6 @@ const ActivitiesSingle = ({ activity: { id, name, description, count, duration, 
         {duration && <div>{`Duration: ${duration}`}</div>}
         { user?.id && location.pathname.includes('edit') &&
         <form className="activities-single-button-form">
-            {/* <button onClick={handleEdit} className='activities-single-edit-button'>Edit</button> */}
             <button onClick={handleDelete} className='activities-single-delete-button'>Delete</button>
         </form>
         }
