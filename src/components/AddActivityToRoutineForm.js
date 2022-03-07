1a33b0aea28ea9fef8b1d74a5091d50139f64684
scburkha005 <scburkha005@gmail.com>
2022-03-06 16:07:28 -0800

    added edit activity fields that properly update their state when changed

--- src/components/AddActivityToRoutineForm.js
+++ src/components/AddActivityToRoutineForm.js
@@ -7,7 +7,8 @@ import * as api from '../api/routinesApi';
 const AddActivityToRoutineForm = ({ routineId, token, editRoutine, setEditRoutine }) => {
   const [message, setMessage] = useState('');
   // init activityId to 1 in the case that the user never changes the field => first activity displayed is always id 1
-  const [routineFields, setRoutineFields] = useState({activityId: 1, count: 0, duration: 0});
+  const [activityAddFields, setActivityAddFields] = useState({activityId: 1, count: 0, duration: 0});
+  const [activityEditFields, setActivityEditFields] = useState({});
 
   const { data: activities } = useQuery('activities', async () => await fetchActivities());
 
@@ -31,18 +32,28 @@ const AddActivityToRoutineForm = ({ routineId, token, editRoutine, setEditRoutin
   });
 
   const handleChange = (e) => {
-    updateState(e, routineFields, setRoutineFields)
+    updateState(e, activityAddFields, setActivityAddFields)
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     // mutate requires a single param or object, so let's build that object with all req info
-    let mutateFields = { ...routineFields, routineId };
+    let mutateFields = { ...activityAddFields, routineId };
     mutate(mutateFields);
   }
 
-  const { activityId, count, duration } = routineFields;
+  const handleEditChange = (e) => {
+    updateState(e, activityEditFields, setActivityEditFields);
+  }
+
+  const handleEditSubmit = async (e) => {
+
+  }
+  console.log(activityEditFields)
+
+  const { activityId, count, duration } = activityAddFields;
+  const { activityEditId, editCount, editDuration } = activityEditFields;
   return (
     <>
       <h2>Add Activities to Routine</h2>
@@ -56,6 +67,22 @@ const AddActivityToRoutineForm = ({ routineId, token, editRoutine, setEditRoutin
         <input value={count} placeholder='count' type='number' onChange={handleChange}/>
         <input value={duration} placeholder='duration' type='number' onChange={handleChange}/>
         <button>Add Activity</button>
+      </form>
+      <h2>Edit Activity</h2>
+      <form onSubmit={handleEditSubmit}>
+        <select name='activityEditId' value={activityEditId} onChange={(e) => {
+          // onChange initialize our activityEditFields state with info from routines
+          // e.target.value = activityId
+          const { id: activityEditId, duration: editDuration, count: editCount } = editRoutine.activities.find(activity => activity.id === Number(e.target.value));
+          setActivityEditFields({activityEditId, editDuration, editCount });
+        }}>
+            {editRoutine.activities?.map(activity => {
+              return <option value={activity.id} key={activity.id}>{activity.name}</option>
+            })}
+        </select>
+        <input value={editCount} placeholder='editCount' type='number' onChange={handleEditChange}/>
+        <input value={editDuration} placeholder='editDuration' type='number' onChange={handleEditChange}/>
+        <button>Edit Activity</button>
       </form>
     </>
   )
