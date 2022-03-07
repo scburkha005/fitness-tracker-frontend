a6bf770946dd58f55ea35d0562e4d04791ddec7b
scburkha005 <scburkha005@gmail.com>
2022-03-06 17:14:34 -0800

    error handling to setState w explicit coerced Number on value

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,6 +1,9 @@
 export const updateState = (e, state, setState) => {
   const { placeholder, name, value } = e.target;
   let keySetter = placeholder || name;
-  console.log(keySetter, value)
+  if (keySetter === 'editCount' || keySetter === 'editDuration') {
+    setState({...state, [keySetter]: Number(value)})
+    return;
+  }
   setState({...state, [keySetter]: value})
 }
